b155d831b2b7ee1eaeea6ab50d709ea438489da7
brent suggs <bsuggs13@outlook.com>
2016-03-07 16:39:13 -0600

    Notes taken
    might want to start over in order to address some issues.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,52 @@
-$(document).ready(function() {
-    
-    $('.gallery').magnificPopup({
-        delegate: 'li a',
-        type: 'image',
-        gallery:{enabled: true}
-    });
-});
-    
+"use strict";
 
 //CREATE VARIABLES 
 var $search = $('#search'); 
+var $gallery = $('.gallery');
 var $imgs =  $('.gallery li img');
 var imgCache = []; 
 
-$imgs.each( function() {
+
+//POSSIBLE OPTION FOR ADDING IMGS TO GALLERY 
+//for (var i = 0; i < imgObjs.length; i += 1) {
+//    var img = imgObjs[i]; 
+//    var html = '<li>'; 
+//    html += '<a href="img/' + img.url + '" title="' + img.title + '">';
+//    html += '<img src="img/Thumbnails/' + img.url + '" alt="' + img.alt + '" />';
+//    html += '</a>';
+//    html += '</li>'; 
+//    
+//    $gallery.append(html); 
+//}
+
+$imgs.each(function () {
     imgCache.push({
         tag: this,
         alt: this.alt.trim().toLowerCase()
-    }) 
+    }); 
 }); 
 
 function filterImgs() {
     var query = $search.val().toLowerCase(); 
 
     imgCache.forEach(function(img) {
-//        if(query){
-            if (img.alt.indexOf(query) === -1) {
-                img.tag.style.display = 'none'; 
-            }else {
-                img.tag.style.display = ''; 
-            }  
-//        }else {
-//            img.tag.style.display = ''; 
-//        }
+        if (img.alt.indexOf(query) === -1) {
+            img.tag.style.display = 'none'; 
+        }else {
+            img.tag.style.display = ''; 
+        }  
     });
 }
 
 
 $search.focus(filterImgs).keyup(filterImgs); 
+
+
+$(document).ready(function() {
+    
+    $gallery.magnificPopup({
+        delegate: 'li a',
+        type: 'image',
+        gallery: {enabled: true}
+    });
+});
