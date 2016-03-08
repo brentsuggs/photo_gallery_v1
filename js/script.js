f4fe4ada08d1da4f42805bea069d97462523472b
brent suggs <bsuggs13@outlook.com>
2016-03-08 16:56:05 -0600

    update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 
 //CREATE VARIABLES 
 var $search = $('#search'); 
+var $gallery = $('#gallery');
 var $overlay = $('<div id="overlay"></div>'); 
 var $img = $('<img>'); 
 var $caption = $('<p></p>'); 
 var $leftArrow = $('<span id="navLeft" class="icon-reply"></span>');
 var $rightArrow = $('<span id="navRight" class="icon-forward"></span>');
+var $removed = []; 
 
 //append img to the overlay
 $overlay.append($img);  
@@ -20,36 +22,104 @@ $overlay.append($leftArrow, $rightArrow);
 //append overlay to body
 $('body').append($overlay); 
 
-
-// Get click event for #gallery links  
-$('#gallery a').click(function(e) {
-    e.preventDefault();
-    
-    var href = $(this).attr('href');
-    var cap = $(this).children('img').attr('alt'); 
-    
+// Open Lightbox with selected image
+function openLightbox(item) {
+    var href = item.children('a').attr('href');
+    var cap = item.children('a').children('img').attr('alt'); 
     $img.attr('src', href); 
     $caption.text(cap); 
-    
-    $overlay.show(); 
+    $overlay.show('slow'); 
+}
+
+// Get click event for #gallery links  
+$('.gallery-item').click(function(e) {
+    e.preventDefault();
+    $(this).addClass('lightbox');
+    openLightbox( $('.lightbox')); 
+
 });
 
 // Get click event for #overlay
-$overlay.click(function() {
-    $overlay.hide(); 
+$img.click(function() {
+    $('.lightbox').removeClass('lightbox');
+    $overlay.hide('slow'); 
 })
 
+//Navigation of Lightbox 
 
 
+function scrollRight() {
+    var $lightbox = $('.lightbox');
+    if($lightbox.is(':last-child')){
+        $lightbox.removeClass('lightbox'); 
+        $('.gallery-item').first().addClass('lightbox'); 
+    } else{
+        $lightbox.removeClass('lightbox').next().addClass('lightbox');
+    }
+    openLightbox($('.lightbox')); 
+}
 
-//$search.focus(filterImgs).keyup(filterImgs); 
+function scrollLeft() {
+    var $lightbox = $('.lightbox');
+    if($lightbox.is(':first-child')){
+        $lightbox.removeClass('lightbox'); 
+        $('.gallery-item').last().addClass('lightbox'); 
+    } else{
+        $lightbox.removeClass('lightbox').prev().addClass('lightbox');
+    }
+    openLightbox($('.lightbox')); 
+}
 
+//keyboard navigation
+$rightArrow.click(function(e) {
+    scrollRight();
+})
 
-//$(document).ready(function() {
-//    
-//    $gallery.magnificPopup({
-//        delegate: 'li a',
-//        type: 'image',
-//        gallery: {enabled: true}
-//    });
+$leftArrow.click(function(e) {
+    scrollLeft();
+})
+
+    
+function keyScroll(e) {
+    if(e.which = 39) {
+        scrollRight(); 
+    }
+    if(e.which = 37) {
+        scrollLeft();
+    }
+}
+
+//$overlay.keydown(function(e) {
+//    keyScroll(e); 
 //});
+
+// Gallery Filtration 
+
+function filterImgs(item, search) {
+    var imgTitle = item.find('img').attr('title'); 
+    return imgTitle.indexOf(search); 
+}
+
+$search.keyup(function() {
+    var searchValue = $search.val().toLowerCase();
+    var result; 
+    $('.gallery-item').each(function() {
+       result =  filterImgs($(this), searchValue); 
+        if(result === -1) {
+            $removed.push($(this)); 
+            $(this).detach(); 
+        }
+    });
+    
+    for (var i = 0; i < $removed.length; i++) {
+        result = filterImgs($removed[i], searchValue);
+        if(result !== -1) {
+            $gallery.append($removed[i]); 
+            $removed.splice(i, 1);
+            i--; 
+        }
+    }
+    
+}); 
+
+
